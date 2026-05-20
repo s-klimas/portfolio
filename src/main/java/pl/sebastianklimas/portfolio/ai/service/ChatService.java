fd5e252d5beb213e0ac36ce66b32fda1d984b336
s-klimas <sebastianklimas@op.pl>
2026-05-20 20:01:00 +0200

    chatbot

---
+++ src/main/java/pl/sebastianklimas/portfolio/ai/service/ChatService.java
@@ -0,0 +1,15 @@
+package pl.sebastianklimas.portfolio.ai.service;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatService {
+    private final ChatClient chatClient;
+
+    public ChatService(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+
+}
