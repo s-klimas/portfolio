3dcd6ee96409cb4de30887d5af4852c40bc57309
s-klimas <sebastianklimas@op.pl>
2026-06-08 19:06:13 +0200

    chat endpoints

--- src/main/java/pl/sebastianklimas/portfolio/ai/controller/ChatController.java
+++ src/main/java/pl/sebastianklimas/portfolio/ai/controller/ChatController.java
@@ -4,13 +4,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.sebastianklimas.portfolio.ai.service.ChatService;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class ChatController {
 
+    ChatService chatService;
+
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    //    @CrossOrigin(origins = "*")
+    @PostMapping("/streamedChat")
+    public Flux<String> streamedChat(@RequestBody String userMessage) {
+        return chatService.getStreamResponse(userMessage);
+    }
+
     //    @CrossOrigin(origins = "*")
     @PostMapping("/chat")
     public ResponseEntity<String> chat(@RequestBody String userMessage) {
-        return ResponseEntity.ok("ok");
+        return ResponseEntity.ok(chatService.getResponse(userMessage));
     }
 }
--- src/main/java/pl/sebastianklimas/portfolio/ai/service/ChatService.java
+++ src/main/java/pl/sebastianklimas/portfolio/ai/service/ChatService.java
@@ -1,15 +1,34 @@
 package pl.sebastianklimas.portfolio.ai.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 @Service
 public class ChatService {
     private final ChatClient chatClient;
 
-    public ChatService(ChatClient.Builder builder) {
-        this.chatClient = builder.build();
+    public ChatService(ChatClient.Builder builder, ChatMemory chatMemory) {
+        var system = "";
+        this.chatClient = builder
+                .defaultSystem(system)
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .build();
     }
 
+    public Flux<String> getStreamResponse(String userMessage) {
+        return chatClient.prompt()
+                .user(userMessage)
+                .stream()
+                .content();
+    }
 
+    public String getResponse(String userMessage) {
+        return chatClient.prompt()
+                .user(userMessage)
+                .call()
+                .content();
+    }
 }
